dc93dd341d38f7245dfe881fac67e3d144d9c9b6
isigen <isidora.abarzua@genstudents.org>
2024-04-26 15:20:19 -0400

    tarea finalizada ahora si

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.getElementById("titulo").innerHTML = "modificando el titulo";
 document.body.style.color="orange";
 
+// const ntitulo= document.createElement("h1")
+// ntitulo.textContent="holia"
+// ntitulo.addEventListener("click",function(){
+//     this.style.color="orange";
+// })
+// document.body.appendChild(ntitulo);
 
 const nuevotitulo=document.createElement("h2");
 nuevotitulo.textContent="Click para cambiar color!!";
-nuevotitulo.addEventListener=("click",function(){
+nuevotitulo.addEventListener("click",function(){
     this.style.color="brown";
 })
 document.body.appendChild(nuevotitulo); //me falta que con el click cambie a cafe
